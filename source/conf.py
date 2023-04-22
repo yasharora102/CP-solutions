01e795f755924ea76e5da652d05f186ca3686c01
Yash Arora <yasharora102@gmail.com>
2023-04-22 20:13:50 +0530

    added document files

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CP - Solutions'
+copyright = '2023, Yash Arora'
+author = 'Yash Arora'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_nb",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+jupyter_execute_notebooks = "off"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "pydata_sphinx_theme"
+html_static_path = ['_static']
+
+
---
+++ source/index.rst
@@ -0,0 +1,10 @@
+
+Welcome to CP - Solutions's documentation!
+==========================================
+    
+   .. toctree::
+      :maxdepth: 2
+      :caption: Contents:
+      :glob:
+
+      notebooks/*
